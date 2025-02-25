e225cf41633e10472ceb5b289cdfe1c6ceb83fbc
MiTSakE247 <ankit.c@mindx360.com>
2025-02-26 01:11:34 +0530

    Initial Commit
    SpringBoot directory setup is initialized

---
+++ src/main/java/com/mdx/achievos/achievos/AchievosApplication.java
@@ -0,0 +1,13 @@
+package com.mdx.achievos.achievos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchievosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchievosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdx/achievos/achievos/AchievosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdx.achievos.achievos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
